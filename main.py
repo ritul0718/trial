6898c2fd4dbf571146791568808365f47d1849a2
ritul0718 <164669584+ritul0718@users.noreply.github.com>
2024-03-28 12:33:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+
+# Sidebar contents
+with st.sidebar:
+    st.title('🤗💬 LLM Chat App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](https://streamlit.io/)
+    - [LangChain](https://python.langchain.com/)
+    - [OpenAI](https://platform.openai.com/docs/models) LLM model
+
+    ''')
+    add_vertical_space(5)
+    st.write('Made with ❤️')
